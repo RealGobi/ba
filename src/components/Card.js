df9471d7fc6e7d0261f10d8e76cd52084f2a9497
Jimmy Svensson <jimmy888swe@gmail.com>
2021-02-23 20:21:27 +0100

    style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import './card.css';
 
-const Card = ({name, number}) => {
-  
+const Card = ({name, number, expires}) => {
+
   return (
     <div className="card">
       <div className="item1">
-      <img src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png" className="card-item__chip" alt="chip" />      </div>
-      <div className="item2"></div>
-      <div className="item3">{number}</div>
-      <div className="item4"></div>
-      <div className="item5"></div>
-      <div className="item6">{name}</div>
-      <div className="item7"></div>
+        <img src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png" className="card-item__chip" alt="chip" />      </div>
+      <div className="item2">
+        <img src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png" alt="visa" className="card-item__typeImg" />
+      </div>
+      <div className="item3">{number ? number :<span><p>####</p><p>####</p><p>####</p><p>####</p></span>}</div>
+      <div className="item4">Card holder</div>
+      <div className="item5">Expires</div>
+      <div className="item6">{name ? name : <p>Name</p>}</div>
+      <div className="item7">{expires ? expires : <p>00-00</p>}</div>
     </div>
   )
 }
--- src/components/MyFrom.js
+++ src/components/MyFrom.js
@@ -4,18 +4,54 @@ import Card from './Card';
 const MyFrom = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
-  const [ccv, setCcv] = useState('');
+  const [expires, setExpires] = useState('');
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
 
   return (
     <div className="my-form">
-      <Card name={name} number={number} />
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" onChange={e=> setName(e.target.value)} />
+      <Card name={name} number={number} expires={expires} />
+      <form onSubmit={submitHandler}>
         <label htmlFor="number">Number:</label>
         <input type="number" name="number" onChange={e=> setNumber(e.target.value)} />
-        <label htmlFor="ccv">CCV:</label>
-        <input type="text" name="ccv" onChange={e=> setCcv(e.target.value)} />
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" onChange={e=> setName(e.target.value)} />
+        <div className="sideBySide">
+          <span className="expires">
+            <label htmlFor="expires">Expiration Date:</label>
+            <select name="expiresMonth" id="expires">
+              <option value="#">Month</option>
+              <option value="01">01</option>
+              <option value="02">02</option>
+              <option value="03">03</option>
+              <option value="04">04</option>
+              <option value="05">05</option>
+              <option value="06">06</option>
+              <option value="07">07</option>
+              <option value="08">08</option>
+              <option value="09">09</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </select>
+            <select name="expiresYear" id="expires">
+              <option value="#">Year</option>
+              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+            </select>
+          </span>
+          <span className="ccv">
+            <label className="ccv" htmlFor="ccv">CCV</label>
+            <input type="number" name="ccv" onChange={e=> setExpires(e.target.value)} />
+          </span>
+        </div>
+        <button name="submit">Submit</button>
       </form>
     </div>
   )
